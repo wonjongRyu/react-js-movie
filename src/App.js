6902f9ee168d20062b0de6ea6d32d49d473d7952
wonjongRyu <wonjong4773@gmail.com>
2019-09-27 18:19:50 +0900

    Component and Props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
 
+function Fruit(props) {
+  return (
+    <h2>
+      I like {props.number} {props.favorite}
+    </h2>
+  );
+}
+
 function App() {
-  return <div> Hello!!</div>;
+  return (
+    <div>
+      <h1>Hello!!</h1>
+      <Fruit favorite="pear" number="2" />
+      <Fruit favorite="grape" number="6" />
+    </div>
+  );
 }
 
 export default App;
