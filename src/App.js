7fb539963f47c17c78ce1d90e8ac871cdf224d3e
wonjongRyu <wonjong4773@gmail.com>
2019-09-28 16:15:18 +0900

    Map and Prop-types

--- src/App.js
+++ src/App.js
@@ -1,19 +1,60 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Fruit(props) {
+function Fruit({ name, number, score }) {
   return (
-    <h2>
-      I like {props.number} {props.favorite}
-    </h2>
+    <div>
+      <h2>
+        I eat {number} {name} everyday! Today, taste of fruit: {score}/5.0
+      </h2>
+    </div>
   );
 }
+
+const fruitILike = [
+  {
+    id: 1,
+    name: "pear",
+    number: 2,
+    score: 4.1
+  },
+  {
+    id: 2,
+    name: "grape",
+    number: 3,
+    score: 3.0
+  },
+  {
+    id: 3,
+    name: "orange",
+    number: 1,
+    score: 4.3
+  },
+  {
+    id: 4,
+    name: "banana",
+    number: 4,
+    score: 3.6
+  }
+];
+
+Fruit.propTypes = {
+  name: PropTypes.string.isRequired,
+  number: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired
+};
 
 function App() {
   return (
     <div>
-      <h1>Hello!!</h1>
-      <Fruit favorite="pear" number="2" />
-      <Fruit favorite="grape" number="6" />
+      {fruitILike.map(fruitILike => (
+        <Fruit
+          key={fruitILike.id}
+          number={fruitILike.number}
+          name={fruitILike.name}
+          score={fruitILike.score}
+        />
+      ))}
     </div>
   );
 }
