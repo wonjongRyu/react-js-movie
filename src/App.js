8b13fc1c0eab49de402bda90e9d81b19a3c12ace
wonjongRyu <wonjong4773@gmail.com>
2019-09-30 08:39:55 +0900

    State

--- src/App.js
+++ src/App.js
@@ -1,62 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Fruit({ name, number, score }) {
-  return (
-    <div>
-      <h2>
-        I eat {number} {name} everyday! Today, taste of fruit: {score}/5.0
-      </h2>
-    </div>
-  );
-}
-
-const fruitILike = [
-  {
-    id: 1,
-    name: "pear",
-    number: 2,
-    score: 4.1
-  },
-  {
-    id: 2,
-    name: "grape",
-    number: 3,
-    score: 3.0
-  },
-  {
-    id: 3,
-    name: "orange",
-    number: 1,
-    score: 4.3
-  },
-  {
-    id: 4,
-    name: "banana",
-    number: 4,
-    score: 3.6
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+  // add 함수 정의
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+  // minus 함수 정의
+  minus = () => {
+    this.setState(current => ({ count: current.count - 1 }));
+  };
+  // render 함수
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
   }
-];
-
-Fruit.propTypes = {
-  name: PropTypes.string.isRequired,
-  number: PropTypes.number.isRequired,
-  score: PropTypes.number.isRequired
-};
-
-function App() {
-  return (
-    <div>
-      {fruitILike.map(fruitILike => (
-        <Fruit
-          key={fruitILike.id}
-          number={fruitILike.number}
-          name={fruitILike.name}
-          score={fruitILike.score}
-        />
-      ))}
-    </div>
-  );
 }
 
 export default App;
