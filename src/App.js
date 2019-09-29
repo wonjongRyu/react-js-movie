b9f0d1c9fd6b896a0fe008be3015cd1c79f74108
wonjongRyu <wonjong4773@gmail.com>
2019-09-30 08:59:26 +0900

    Life Cycle

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 class App extends React.Component {
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
   };
-  // add 함수 정의
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-  // minus 함수 정의
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
-  };
-  // render 함수
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  }
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
   render() {
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
